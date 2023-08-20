19089e36e9c73579665515f5545e18cf31447e3f
RenanCecchin <renan.cecchin@hotmail.com>
2023-08-20 12:52:25 -0300

    Fix font scale

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from recognition import yolo_predictor
-import cv2 as cv
+import  cv2 as cv
 import numpy as np
 from PIL import Image
 
@@ -18,9 +18,8 @@ def main():
             coords = box.xyxy[0].tolist()
             coords = [round(x) for x in coords]
             conf = round(box.conf[0].item(), 2)
-            scale = 0.5
+            scale = 0.9
             #scale = min(recognized_img.shape[0], recognized_img.shape[1]) / (25/scale)
-            st.write(scale)
             cv.rectangle(recognized_img, (coords[0], coords[1]), (coords[2], coords[3]), (0, 255, 0), 2)
             cv.putText(recognized_img, f"{class_id} {conf}", (coords[0], coords[1]), cv.FONT_HERSHEY_SIMPLEX, scale, (0, 255, 0), 2)
         st.image(recognized_img, channels="RGB")
